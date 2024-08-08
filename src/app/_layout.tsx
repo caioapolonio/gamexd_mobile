3b5793975e4fee039ba12f5d74ac444d9b0107a6
Caio <cvmaaa1@gmail.com>
2024-08-08 15:42:27 -0300

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import "../styles/global.css";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
